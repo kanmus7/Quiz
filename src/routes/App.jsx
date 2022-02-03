bca9a1f8d2e5d693fe6c427285565601ed40d758
unknown <sebastiangc.2309@gmail.com>
2022-02-03 10:19:08 -0500

    changes in the app for deploy config

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,14 +4,14 @@ import HomeScreen from "../components/HomeScreen";
 import QuizzesLayout from "../components/quizScreen/QuizzesLayout";
 import ResultsLayout from "../components/resultScreen/ResultsLayout"
 
-function App() {  
+function App() {
   const [answer, setAnswer] = useState([])
   const [quizzes, setQuizzes] = useState([])
   return (
     <Routes>
-     <Route path='/' element={<HomeScreen setAnswer={setAnswer} />} />
-     <Route path='/quizzes' element={<QuizzesLayout answer={answer} setAnswer={setAnswer} quizzes={quizzes} setQuizzes={setQuizzes}/>} />
-     <Route path='/results' element={<ResultsLayout  quizzes={quizzes} answers={answer} setAnswer={setAnswer}/>} />
+      <Route path="/" element={<HomeScreen setAnswer={setAnswer} />} />
+      <Route path="/quizzes" element={<QuizzesLayout answer={answer} setAnswer={setAnswer} quizzes={quizzes} setQuizzes={setQuizzes} />} />
+      <Route path="/results" element={<ResultsLayout quizzes={quizzes} answers={answer} setAnswer={setAnswer} />} />
     </Routes>
   );
 }
